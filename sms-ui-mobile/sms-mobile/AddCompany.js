6f81439f65fca231c4d572596b72142b4b0d7ba3
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-01-09 19:00:29 +0530

    login Ui updated and navigation between screens are added

--- sms-ui-mobile/sms-mobile/AddCompany.js
+++ sms-ui-mobile/sms-mobile/AddCompany.js
@@ -1,37 +1,43 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 import styles from "./style/AddCompanyStyle";
-import { createStackNavigator } from "@react-navigation/stack";
 import { RectangleBox } from "./RectangleBoc";
 const addComIcon = require("./icon/fi-rr-plus.png");
-const userIcon = require("./icon/fi-bs-user.png");
 
 export default function AddCompany({ navigation }) {
   return (
-    <View style={styles.mainContainer}>
-      <Text style={styles.usernames}>Logged in As- Username (Admin)</Text>
-      <View>
-        <Text style={styles.textCompany}>Companies</Text>
-      </View>
-      <View style={styles.secondContainer}>
-        <View style={styles.thirdContainer}></View>
+    <ScrollView>
+      <View style={styles.mainContainer}>
+        <Text style={styles.usernames}>Logged in As- Username (Admin)</Text>
+        <View>
+          <Text style={styles.textCompany}>Companies</Text>
+        </View>
+        <View style={styles.secondContainer}>
+          <View style={styles.thirdContainer}></View>
 
-        <View style={styles.containerCompany}>
-          <TouchableOpacity>
-            <View style={styles.rectangleBox}>
-              <Image style={styles.addIcon} source={addComIcon} />
-            </View>
-          </TouchableOpacity>
-          <RectangleBox />
-          <RectangleBox />
-          <RectangleBox />
-        </View>
-        <View style={styles.containerCompany}>
-          <RectangleBox />
-          <RectangleBox />
-          <RectangleBox />
-          <RectangleBox />
+          <View style={styles.containerCompany}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("RegisterScreen")}
+            >
+              <View style={styles.rectangleBox}>
+                <Image style={styles.addIcon} source={addComIcon} />
+              </View>
+            </TouchableOpacity>
+            <RectangleBox />
+          </View>
+          <View style={styles.containerCompany}>
+            <RectangleBox />
+            <RectangleBox />
+          </View>
+          <View style={styles.containerCompany}>
+            <RectangleBox />
+            <RectangleBox />
+          </View>
+          <View style={styles.containerCompany}>
+            <RectangleBox />
+            <RectangleBox />
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
--- sms-ui-mobile/sms-mobile/login.js
+++ sms-ui-mobile/sms-mobile/login.js
@@ -1,37 +1,73 @@
 import React, { useState } from "react";
 import { MyTextInput } from "./component/MyTextInput";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import styles from "./style/MyStyle";
-export default function LoginScreen() {
+const backgroundImg = require("./icon/Ellipse.png");
+const logoApple = require("./icon/logoApple.png");
+const logoGoogle = require("./icon/logoGoogle.png");
+const logoFb = require("./icon/logoFb.png");
+export default function LoginScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={{ textAlign: "center", fontSize: 25, marginTop: 20 }}>
-        Survey Management System
-      </Text>
-      <Text style={{ textAlign: "center", fontSize: 25, marginBottom: 80 }}>
-        Log In
-      </Text>
+    <View style={styles.containerMain}>
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center", fontSize: 25, marginTop: 20 }}>
+          Survey Management System
+        </Text>
+        <Text style={{ textAlign: "center", fontSize: 25, marginBottom: 80 }}>
+          Log In
+        </Text>
 
-      <Text style={{ marginBottom: 5 }}>Email</Text>
-      <MyTextInput
-        myPlaceHolder={"Enter your email"}
-        kBoardType={"email-address"}
-        autoCompType={"email"}
-      />
-      <Text style={{ marginBottom: 5 }}>Password</Text>
-      <MyTextInput
-        myPlaceHolder={"Enter your password"}
-        kBoardType={"numeric"}
-        autoCompType={"password"}
-      />
-      <Text style={{ textAlign: "right", fontSize: 12 }}>Forgot Password</Text>
+        <Text style={{ marginBottom: 5 }}>Email</Text>
+        <MyTextInput
+          myPlaceHolder={"Enter your email"}
+          kBoardType={"email-address"}
+          autoCompType={"email"}
+        />
+        <Text style={{ marginBottom: 5 }}>Password</Text>
+        <MyTextInput
+          myPlaceHolder={"Enter your password"}
+          kBoardType={"numeric"}
+          autoCompType={"password"}
+        />
+        <Text style={{ textAlign: "right", fontSize: 12 }}>
+          Forgot Password
+        </Text>
 
-      <TouchableOpacity style={styles.loginButton}>
-        <Text style={{ color: "#fff" }}>Log in</Text>
-      </TouchableOpacity>
-      <Text style={{ ...styles.heading1, ...{ textAlign: "center" } }}>
-        Haven’t got an account? Sign up
-      </Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("AddCompany")}
+          style={styles.loginButton}
+        >
+          <Text style={{ color: "#fff" }}>Log in</Text>
+        </TouchableOpacity>
+        <Text style={{ ...styles.heading1, ...{ textAlign: "center" } }}>
+          Haven’t got an account? Sign up
+        </Text>
+        <View style={styles.containerLoginOp}>
+          <Text>Or sign in with</Text>
+          <View
+            style={{
+              ...styles.containerLoginOp,
+              ...{ marginStart: 100 },
+            }}
+          >
+            <OverLayImg overImg={logoGoogle} />
+            <OverLayImg overImg={logoFb} />
+            <OverLayImg overImg={logoApple} />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
+
+const OverLayImg = ({ overImg }) => {
+  return (
+    <TouchableOpacity style={{ marginLeft: 5 }}>
+      <Image source={backgroundImg} />
+      <Image
+        source={overImg}
+        style={{ position: "absolute", marginLeft: 12, marginTop: 10 }}
+      />
+    </TouchableOpacity>
+  );
+};
