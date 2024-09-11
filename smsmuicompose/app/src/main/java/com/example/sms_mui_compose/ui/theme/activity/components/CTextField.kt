b6041458439bc5335275897539473561906eea00
Rahul Pahuja <rahulpahuja2015@gmail.com>
2024-09-11 16:37:54 +0530

    Added Margin around the fields

--- smsmuicompose/app/src/main/java/com/example/sms_mui_compose/ui/theme/activity/components/CTextField.kt
+++ smsmuicompose/app/src/main/java/com/example/sms_mui_compose/ui/theme/activity/components/CTextField.kt
@@ -1,6 +1,8 @@
 package com.example.sms_mui_compose.ui.theme.activity.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -11,18 +13,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun CTextField(label:String) {
     var text by remember { mutableStateOf("") }
-    OutlinedTextField(
-        value = text,
-        onValueChange = { newEmail ->
-            text = newEmail
-        },
-        label = { Text(label) },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-        modifier = Modifier.fillMaxWidth(),
-    )
+    Box(modifier = Modifier.padding(12.dp)) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newEmail ->
+                text = newEmail
+            },
+            label = { Text(label) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
 }
